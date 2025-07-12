1f69425f5aaa82a0c2da3369f7a0e33d10be1a0d
Hasan Mukhlis <hsnmkls@proton.me>
2025-07-12 22:15:01 +0800

    adding threlte

---
+++ apps/web-frontend/src/lib/game/scene.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { Gizmo, OrbitControls } from '@threlte/extras';
+</script>
+
+<T.PerspectiveCamera position={[10, 15, 10]} makeDefault lookAt.y={0}>
+	<OrbitControls />
+	<Gizmo></Gizmo>
+</T.PerspectiveCamera>
+
+<T.Mesh>
+	<T.BoxGeometry></T.BoxGeometry>
+	<T.MeshBasicMaterial></T.MeshBasicMaterial>
+</T.Mesh>
--- apps/web-frontend/src/routes/+page.svelte
+++ apps/web-frontend/src/routes/+page.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
+	import Scene from '$lib/game/scene.svelte';
+	import { Canvas } from '@threlte/core';
+
 	const { data } = $props();
 </script>
 
-{#if data.user}
-	<div>
-		logged in as {data.user.address}
-		{data.user.email ? `(${data.user.email})` : ''}
+<div class="flex h-screen w-full items-stretch">
+	<div class="max-w-xs p-4">
+		{#if data.user}
+			<div class="truncate text-sm">
+				logged in as {data.user.address}
+				{data.user.email ? `(${data.user.email})` : ''}
+			</div>
+			<form action="?/logout" method="POST">
+				<button>Logout</button>
+			</form>
+		{:else}
+			<form action="?/login" method="POST">
+				<button>Login</button>
+			</form>
+		{/if}
 	</div>
-	<form action="?/logout" method="POST">
-		<button>Logout</button>
-	</form>
-{:else}
-	<form action="?/login" method="POST">
-		<button>Login</button>
-	</form>
-{/if}
+	<div class="flex-1">
+		<Canvas>
+			<Scene />
+		</Canvas>
+	</div>
+</div>
